b22e20538ceef5a15169819da97318e0c5e0eb56
PeytenBevill <redsage00@gmail.com>
2023-12-12 14:17:25 -0600

    dealing with CORS issues

--- index.js
+++ index.js
@@ -26,6 +26,8 @@ app.use(function (req, res, next) {
   // Request headers you wish to allow
   res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, content-type");
 
+  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+  res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g., in case you use sessions)
   res.setHeader("Access-Control-Allow-Credentials", true);
